785a3eaeefdb5437289d1a065969f565c0d29c4b
Leonardo Diniz <164882409+ldinizbr@users.noreply.github.com>
2025-04-23 13:36:48 -0300

    Add a .py file

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem
+# 
+# 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized. We show the tasks we can do as create a markdown cell and code lines.
+
+# **Objectives:**
+# 
+# - List the popular languages on Data Scienci
+# - Libraries used by Data Scientists
+# - Some tools used by Data Scientists
+# - Exemples of arithmetic expressions
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 5. JavaScript
+# 6. Java / Scala
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# **Pyton**
+# 1. Pandas
+# 2. numpy
+# 3. matplotlib
+# 4. seaborn
+# 5. scikit-learn
+# 6. TensorFlow
+# 7. PyTorch
+# 8. Keras
+# 
+# **R**
+# 1. dplyr
+# 2. data.table
+# 3. ggplot
+# 4. plotly
+# 5. caret
+
+# | Data Science Tools |
+# | ------------------ |
+# | MySql |
+# | MongoDB |
+# | Apache Hadoop |
+# | Apache Kafka |
+# | GitHub |
+# | Jupyter Notebooks |
+# | RStudio |
+# | PowerBI |
+# | Tableau |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[2]:
+
+
+#This a simple arithmetic expression to mutiply then add integers.
+
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60.
+
+200/60
+
+
+# ## Author
+# Leonardo Diniz
+# 
+
+# In[ ]:
+
+
+
+
